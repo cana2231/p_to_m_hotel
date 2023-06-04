d7d035d1eb1ead7b0a758340877f323a96200fd5
Cana2231 <cana2231@gmail.com>
2023-06-04 11:37:09 +0900

    change design

---
+++ src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class LoginController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index () {
+        return view('login/index');
+    }
+
+    // public function login(Request $request)
+    // {
+
+    //     // var_dump($request);
+    //     // exit;
+
+
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'email' => 'required|email|unique:users',
+    //         'phone' => 'required|numeric',
+    //         'address' => 'required',
+    //         'password' => 'required|min:6',
+    //     ]);
+
+    //     $user = new User;
+    //     $user->name = $request->input('name');
+    //     $user->email = $request->input('email');
+    //     $user->phone = $request->input('phone');
+    //     $user->address = $request->input('address');
+    //     $user->password = Hash::make($request->input('password'));
+    //     $user->save();
+
+    //     return response()->json(['message' => 'ユーザー登録が完了しました。'], 201);
+    //     //TODO ログインをしてTOPへ遷移させる
+    // }
+
+}
---
+++ src/resources/views/login/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>P to M's Hotelログイン</title>
+    <link rel="stylesheet" type="text/css" href="css/login/index.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="login-container">
+        <form action="">
+          <h2>会員ログイン</h2>
+          <div class="input-container">
+            <i class="fa fa-envelope icon"></i>
+            <input type="text" placeholder="メールアドレス" />
+          </div>
+          <div class="input-container">
+            <i class="fa fa-lock icon"></i>
+            <input type="password" placeholder="パスワード" />
+          </div>
+          <button type="submit">ログイン</button>
+        </form>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
